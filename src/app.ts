e6e99b9aff961c3f9eeed13708181b57bfda4cc6
nyanyaonn <nyanyaonn@gmail.com>
2023-01-12 18:43:12 +0800

    update view and server

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,13 @@ import { contentType } from "https://deno.land/std@0.170.0/media_types/mod.ts";
 const port = 80;
 const HOST = `http://localhost${port === 80 ? "" : ":" + port}`;
 const handler = (request: Request) => {
-  const base = request.url.replace(HOST, "");
-  if (base === "/") {
+  // const base = request.url.replace(HOST, "");
+  const base = new URL(request.url);
+  const path = base.pathname;
+  console.log(path);
+  if (path === "/") {
     const body = Deno.readTextFileSync("./public/index.html");
-    return new Response(body + request.url, {
+    return new Response(body, {
       status: 200,
       headers: {
         "content-type": "text/html; charset=utf-8",
@@ -15,8 +18,8 @@ const handler = (request: Request) => {
     });
   }
 
-  const file_type: string = base.slice(base.indexOf("."), base.length);
-  return new Response(Deno.readTextFileSync("./public/" + base), {
+  const file_type: string = path.slice(path.indexOf("."), path.length);
+  return new Response(Deno.readTextFileSync("./public/" + path), {
     status: 200,
     headers: {
       "content-type": contentType(file_type) as string,
